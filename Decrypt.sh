8187dc4eea62e218c309f26734bdd498eb860e24
Ccook4Umass <60358528+Ccook4Umass@users.noreply.github.com>
2020-12-17 22:55:55 -0500

    Update Decrypt.sh

--- Decrypt.sh
+++ Decrypt.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+start=`date +%s%N`
+file=$1
+algo=$2
+key=$3
+c='CC'
+cc='CC'
+v='VC'
+vc='VC'
 
+if [[ $algo == $cc ]]
+then
+        python CC_Decrypt.py $key
+elif [[ $algo == $vc ]]
+then
+        python VC_Decrpyt.py $key
+fi
+end=`date +%s.%N`
