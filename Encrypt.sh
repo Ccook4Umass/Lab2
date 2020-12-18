463229b4293b911521f51adbfcb23a3407fbf411
Ccook4Umass <60358528+Ccook4Umass@users.noreply.github.com>
2020-12-17 23:08:15 -0500

    Update Encrypt.sh

--- Encrypt.sh
+++ Encrypt.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-file=$1
-algo=$2
-key=$3
-c='CC'
-cc='CC'
-v='VC'
-vc='VC'
 
-if [[ $algo == $cc ]]
-then
-	python CC_Encrypt.py $key
-elif [[ $algo == $vc ]]
-then
-	python VC_Encrypt.py $key
-fi
